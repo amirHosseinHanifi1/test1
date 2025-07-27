84efc687eeef40ee4d551eae0e2316d6c30d3531
amirHosseinHanifi1 <amirhanifi.1381@gmail.com>
2025-07-27 10:07:24 +0330

    part #9

--- js/User.js
+++ js/User.js
@@ -1,4 +1,15 @@
   document.addEventListener('DOMContentLoaded', function () {
       var elems = document.querySelectorAll('.collapsible');
       var instances = M.Collapsible.init(elems, {});
+
+      const addUserButton = document.getElementById ('add-user-button')
+      addUserButton.addEventListener ('click', ()=>{
+        document.querySelector ('.add-user-modal').classList.add ('show')
+        document.querySelector ('.add-user-modal-back').classList.remove ('display-none')
+      })
+
+      document.querySelector ('.add-user-modal-back').addEventListener ('click', (e)=>{
+        document.querySelector ('.add-user-modal').classList.remove ('show')
+        e.target.classList.add ('display-none')
+      })
   });
